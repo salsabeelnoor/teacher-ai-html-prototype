d273b5930977f12d9f29cbe3946890cac441555b
Salsabeel Noor <salsabeelnoor@gmail.com>
2024-08-04 03:02:42 +0600

    language page updated

--- js/footer-nav.js
+++ js/footer-nav.js
@@ -1,50 +1,33 @@
 "use strict";
 
 /*----------------- nav bar variables --------------------*/
-const navLinks = document.querySelectorAll(".side-nav a");
-const sections = document.querySelectorAll("section");
-const navLinksMb = document.querySelectorAll("nav a");
+const navbar = document.getElementById("top-nav");
+const navbarMobile = document.getElementById("nav-mobile");
+const offcanvas = document.getElementById("offcanvasExample");
+const offcanvasBtn = document.getElementById("offcanvas-btn");
 
-function scrollChange(navLinks) {
-  let currentSection = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      currentSection = sections[sections.length - 1].id;
-    } else if (
-      window.scrollY >= sectionTop - sectionHeight / 2 &&
-      window.scrollY < sectionTop + sectionHeight
-    ) {
-      currentSection = section.id;
-    }
-  });
 
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href").substring(1) === currentSection) {
-      link.classList.add("active");
+
+function navScrollFunction() {
+    if (document.documentElement.scrollTop > 0 ) {
+      navbar.classList.add("on-scroll");
+      navbar.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+      navbar.style.transition = "all 0.24s ease-in"
+      navbarMobile.classList.add("on-scroll");
+      navbarMobile.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+      navbarMobile.style.transition = "all 0.24s ease-in"
+
     }
-    /*---------------------------------------------------- */
-    if (window.innerWidth <= 992) {
-      link.addEventListener("click", function (e) {
-        e.preventDefault();
-        const href = link.getAttribute("href");
-        // Scroll to other links
-        if (href !== "#" && href.startsWith("#")) {
-          const sectionEl = document.querySelector(href);
-          window.scrollTo({
-            behavior: "smooth",
-            top:
-              sectionEl.getBoundingClientRect().top -
-              document.body.getBoundingClientRect().top -
-              50,
-          });
-        }
-      });
+    else {
+      navbar.classList.remove("on-scroll");
+      navbar.style.backgroundColor = "transparent";
+      navbarMobile.classList.remove("on-scroll");
+      navbarMobile.style.backgroundColor = "transparent";
+      // if(screen.width <  992 && !offcanvas.classList.contains('show')){
+      // }
     }
-  });
+
 }
 document.addEventListener("scroll", () => {
-  window.innerWidth <= 992 ? scrollChange(navLinksMb) : scrollChange(navLinks);
+  navScrollFunction();
 });
