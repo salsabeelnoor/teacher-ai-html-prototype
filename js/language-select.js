0e553cf48c21318c4d536210d153a3fbedb6de2c
Salsabeel Noor <salsabeelnoor@gmail.com>
2024-08-12 18:07:26 +0600

    updated

--- js/language-select.js
+++ js/language-select.js
@@ -3,7 +3,10 @@ const selectBtn = document.getElementById('select-btn');
 const infoBtn = document.getElementById('info-btn');
 const description = document.getElementById('description-box');
 const languageMaterial = document.getElementById('language-material');
-
+const languageOutput = document.getElementById('language-output');
+const startBtn = document.getElementById('start-btn');
+const startCircle = document.querySelector('.circle');
+console.log(startCircle)
 selectBtn.addEventListener('click', () => {
     languageMaterial.classList.toggle('d-none');
     description.classList.add('d-none');
@@ -11,4 +14,14 @@ selectBtn.addEventListener('click', () => {
 infoBtn.addEventListener('click', () => {
     description.classList.toggle('d-none');
     languageMaterial.classList.add('d-none');
+})
+startBtn.addEventListener('click', () => {
+    startCircle.classList.toggle('circle--green');
+    languageOutput.classList.toggle('d-none');
+    if(startBtn.hasAttribute("disabled")){
+        startBtn.removeAttribute('disabled')
+    }
+    else{
+        startBtn.setAttribute('disabled', '')
+    }
 })
